55ecc84c449fb4eae381d63c7f58403437fdce61
Jan Kremen <kremenh@gmail.com>
2019-07-30 02:29:28 +0200

    work on nats

--- main.ts
+++ main.ts
@@ -2,11 +2,15 @@ import {Client, connect, Payload} from 'ts-nats';
 
 async function start() {
     let nc: Client = await connect({servers: ["localhost:4222"], payload: Payload.JSON});
-    await nc.subscribe("test", (err, msg) => {
+    await nc.subscribe("test.*.*", (err, msg) => {
         console.log(msg.data.message);
         nc.publish(msg.reply!, {message: "ok"})
-    }, {max: 1});
-    let result = await nc.request("test", 1000, {message: "Hello"});
+    },{queue: 'a'});
+    await nc.subscribe("test.a.b.c", (err, msg) => {
+        console.log(msg.data.message);
+        nc.publish(msg.reply!, {message: "ok"})
+    },{queue: 'b'});
+    let result = await nc.request("test.a.b.c", 1000, {message: "Hello"});
     console.log(result.data.message);
 }
 
